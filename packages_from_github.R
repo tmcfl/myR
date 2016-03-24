36e62f1057838ad268e24b4562abf4832a3bfdde
Timothy McFall <timothy.mcfall@gmail.com>
2016-03-24 10:14:00 -0600

    added new packages

--- packages_from_github.R
+++ packages_from_github.R
@@ -1,4 +1,11 @@
 
+# purrr
+devtools::install_github("hadley/purrr")
 
-# ggThemeAssist - RStudio Addin
+# RSTUDIO ADDINS ----------------------------------------------------------
+
+# ggThemeAssist
 devtools::install_github("calligross/ggthemeassist")
+
+# tidyshiny - use tidyr interactively to gather columns into rows - tidyData(iris)
+devtools::install_github("MangoTheCat/tidyshiny")
