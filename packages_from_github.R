b28852c95e7c41a0fc11df5e0ed85885813b5043
Timothy McFall <timothy.mcfall@gmail.com>
2016-06-20 17:49:14 -0600

    added additional packages

--- packages_from_github.R
+++ packages_from_github.R
@@ -2,10 +2,24 @@
 # purrr
 devtools::install_github("hadley/purrr")
 
+# htmlwidgets
+devtools::install_github("ramnathv/htmlwidgets")
+
+# rpivotTable
+devtools::install_github("smartinsightsfromdata/rpivotTable")
+
 # RSTUDIO ADDINS ----------------------------------------------------------
+# https://github.com/daattali/addinslist
+
+# addins manager
+devtools::install_github('rstudio/DT')
+devtools::install_github("csgillespie/addinmanager")
 
 # ggThemeAssist
 devtools::install_github("calligross/ggthemeassist")
 
 # tidyshiny - use tidyr interactively to gather columns into rows - tidyData(iris)
 devtools::install_github("MangoTheCat/tidyshiny")
+
+# rpivotGadget
+devtools::install_github("dkilfoyle/rpivotGadget")
